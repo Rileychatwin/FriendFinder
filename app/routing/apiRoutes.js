ea7d6185e00d701d5ade3d952f6d5b7c896c6d87
Riley <rileychatwin@outlook.com>
2020-08-06 00:29:40 -0600

    added code for api calls

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,53 @@
 var friends = require("../data/friends");
 
+var zeMatch = {
+    name: "",
+    photo: "",
+    friendDifference: Infinity
+};
+
+
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    app.post("/api/friends", function(req, res) {
-        var zeMatch = {
-            name: "",
-            photo: "",
-            friendDifference: Infinity
-          };
-          
+    app.post("/api/friends", function (req, res) {
+
+
+        var userSurv = req.body;
+        var survScore = userSurv.scores;
+
+        theDifference(survScore);
+
+        friends.push(userSurv);
+
+        res.json(bestMatch);
+
     })
 
+    function theDifference(scores) {
+
+        for (var i = 0; i < friends.length; i++) {
+            var thisFriend = friends[i];
+            var totalDifference = 0
+
+            for (var x = 0; x < thisFriend.scores.length; x++) {
+                var thisFriendScore = thisFriend.scores[x];
+                var thisUserScore = scores[x];
+
+                totalDifference += Math.abs(parseInt(thisUserScore) - parseInt(thisFriendScore));
+            }
+
+            yourMatch(totalDifference,thisFriend);
+        }
+    }
+
+    function yourMatch(totalDifference, thisFriend){
+        if (totalDifference <= bestMatch.friendDifference) {
+            bestMatch.name = thisFriend.name;
+            bestMatch.photo = thisFriend.photo;
+            bestMatch.friendDifference = totalDifference;
+          }
+    }
 }
