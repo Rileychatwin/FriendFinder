04978b9d66a47d062a43a4a475061bdf4a5d457d
Riley <rileychatwin@outlook.com>
2020-08-05 23:30:58 -0600

    added server.js code

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+    {
+      name: "Ahmed",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+        name: "Jeff",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        scores: [
+          "7",
+          "2",
+          "1",
+          "8",
+          "2",
+          "1",
+          "4",
+          "4",
+          "4",
+          "1"
+        ]  
+    }
+]
+
+module.exports = friends;
